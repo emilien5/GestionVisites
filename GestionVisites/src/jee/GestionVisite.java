962a662ad7a95722a104146f5accd48334b8acb5
lubuntu <lubuntu@lubuntu-VirtualBox>
2018-03-16 17:33:12 +0100

    Validation de la méthode trouverVisite

--- GestionVisites/src/jee/GestionVisite.java
+++ GestionVisites/src/jee/GestionVisite.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 
 public class GestionVisite {
 	
-	public ArrayList<Visite> trouverVisite(Visite uneVisite){
+	public static ArrayList<Visite> trouverVisite(Visite uneVisite){
 		
 		ArrayList<Visite> listeVisite = new ArrayList<Visite>();
 		ArrayList<Visite> listeVisiteParCategorie = new ArrayList<Visite>();
 		
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			Connection db = DriverManager.getConnection("jdbc:mysql://localhost/GestionVisite?user=root&password=network");
+			Connection db = DriverManager.getConnection("jdbc:mysql://localhost/GestionVisite?user=admin&password=network");
 			Statement stmt = db.createStatement();
 			stmt.executeQuery("Select * from Visite");
 			ResultSet rset = stmt.getResultSet();
@@ -24,31 +24,66 @@ public class GestionVisite {
 			
 			while(rset.next()) {
 				Visite visite = new Visite();
-				visite.setDateVisite(("dateVisite").toString());
-				visite.setIdVisite(Integer.parseInt("idVisite"));
-				visite.setPrixVisite(Integer.parseInt("prixVisite"));
-				visite.setTypeVisite(("typeVisite").toString());
-				visite.setVille(("ville").toString());
+				visite.setDateVisite((rset.getString("dateVisite")).toString());
+				visite.setIdVisite(Integer.parseInt(rset.getString("idVisite")));
+				visite.setPrixVisite(Integer.parseInt(rset.getString("prixVisite")));
+				visite.setTypeVisite((rset.getString("typeVisite")).toString());
+				visite.setVille((rset.getString("ville")).toString());
+				System.out.println(rset.getString("ville"));
 				listeVisite.add(visite);
 			}
 			
-			for(int i=0; i<listeVisite.size()-1; i++) {
-				if(listeVisite.get(i).getTypeVisite() == uneVisite.getTypeVisite() ||
-						listeVisite.get(i).getDateVisite() == uneVisite.getDateVisite() ||
-						listeVisite.get(i).getVille() == uneVisite.getVille()) {
+			for(int i=0; i<listeVisite.size(); i++) {
+				if(listeVisite.get(i).getTypeVisite().equals(uneVisite.getTypeVisite()) ||
+						listeVisite.get(i).getDateVisite().equals(uneVisite.getDateVisite()) ||
+						listeVisite.get(i).getVille().equals(uneVisite.getVille())) {
 					listeVisiteParCategorie.add(listeVisite.get(i));
 				}
 			}
 			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(listeVisiteParCategorie);
+		return listeVisiteParCategorie;
+				
+	}
+	
+	public static void main(String[] arg0) {
+		Visite visite = new Visite();
+		visite.setVille("Toulouse");
+		trouverVisite(visite);
+	}
+	
+	/*public String annulerVisite(int codeReservation) {
+		
+		int codeReservationVisite = 0;
+		String message = "";
+		
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Connection db = DriverManager.getConnection("jdbc:mysql://localhost/GestionVisite?user=admin&password=network");
+			Statement stmt = db.createStatement();
+			stmt.executeQuery("Select idReservation from Reservation WHERE idReservation ='" + codeReservation + "'");
+			ResultSet rset = stmt.getResultSet();
+
+			codeReservationVisite = (int) rset.getObject(1);
 			
+			if(codeReservationVisite != 0) {
+				stmt.executeQuery("UPDATE Reservation SET )
+				message = "Vous avez annulé votre visite";
+			}
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return listeVisiteParCategorie;
-				
-	}
+		
+		
+		
+		return message;
+	}*/
 }
 	
